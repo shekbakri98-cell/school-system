096c6cdb2562fe8122bac18f833e2105b20e31d3
shekbakri98-cell <shekbakri98@gmail.com>
2026-08-20 02:40:24 +0300

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,13 +1,15 @@
 <?php
-// Odeeffannoo sarvarii Alwaysdata keetii isa sirrii
 $servername = "mysql-anewar.alwaysdata.net"; 
-$username = "anewar_admin"; // User yommuu uumtu 'anewar_' itti dabalama
-$password = "015661Emran@";      // Iccitii USERS irratti uumte galchi
-$dbname = "anewar_school_db"; // Maqaa database keetii guutuu
+$username = "anewar_admin"; 
+$password = "015661Emran@";      
+$dbname = "anewar_school_db"; 
 
+// Walitti hidhamiinsa uumuu
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Walitti hidhamuu isaa mirkaneessuu
 if ($conn->connect_error) {
     die("Kuusaa odeeffannoo waliin walitti hidhuun hin danda'amne: " . $conn->connect_error);
 }
+echo "Kuusaa odeeffannoo (Database) waliin milkiidhaan walitti hidhameera!";
 ?>

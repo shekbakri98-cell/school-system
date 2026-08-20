c2d8dab3c4e60b15853be67b255857221f068788
shekbakri98-cell <shekbakri98@gmail.com>
2026-08-20 18:43:19 +0300

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,15 +1,18 @@
+
+?>
 <?php
 $servername = "mysql-anewar.alwaysdata.net"; 
 $username = "anewar_admin"; 
 $password = "015661Emran@";      
 $dbname = "anewar_school_db"; 
 
-// Walitti hidhamiinsa uumuu
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Walitti hidhamuu isaa mirkaneessuu
 if ($conn->connect_error) {
     die("Kuusaa odeeffannoo waliin walitti hidhuun hin danda'amne: " . $conn->connect_error);
 }
-//echo "Kuusaa odeeffannoo (Database) waliin milkiidhaan walitti hidhameera!";
+
+$msg = "";
+$msg_type = "";
+$page = isset($_GET['page']) ? $_GET['page'] : 'student_form';
 ?>
